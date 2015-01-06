d282bfe31126c9fae9081e5cfbae2b2a3a3e7683
hernani <hernani.fernandes@fcmoreno.com.br>
2015-01-06 10:06:47 -0200

    Initi Aula

---
+++ src/br/com/aula1/beans/Aula1MB.java
@@ -0,0 +1,26 @@
+package br.com.aula1.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="aula")
+@SessionScoped
+public class Aula1MB {
+	
+	private String nome;
+	private final String CONSTANTE = "Essa é uma constante";
+	
+	public Aula1MB(){
+		nome = "hernani  " + CONSTANTE;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+		
+}
